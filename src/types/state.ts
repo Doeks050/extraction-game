70ba5fbf2c4664b1a3c3818ca6ee355fe3db7d84
Doeks050 <nicktalens@outlook.com>
2026-06-17 21:26:00 +0200

    Add central game state type

---
+++ src/types/state.ts
@@ -0,0 +1,12 @@
+import type { HideoutModule, OperatorProfile } from "./game";
+import type { InventorySlot } from "./items";
+import type { CurrentLoadout } from "./loadout";
+import type { GameTask } from "./tasks";
+
+export type GameState = {
+  operator: OperatorProfile;
+  hideoutModules: HideoutModule[];
+  stash: InventorySlot[];
+  loadout: CurrentLoadout;
+  tasks: GameTask[];
+};
