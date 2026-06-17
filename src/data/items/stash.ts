b018418a86d7eac290295a8692dba65076eaf3c9
Doeks050 <nicktalens@outlook.com>
2026-06-17 20:01:11 +0200

    Add starter stash inventory

---
+++ src/data/items/stash.ts
@@ -0,0 +1,20 @@
+import type { InventorySlot } from "../../types/items";
+
+export const starterStash: InventorySlot[] = [
+  { slotId: "stash_001", itemId: "wpn_m4a1", quantity: 1 },
+  { slotId: "stash_002", itemId: "wpn_glock19x", quantity: 1 },
+  { slotId: "stash_003", itemId: "ammo_556_m855a1", quantity: 60 },
+  { slotId: "stash_004", itemId: "ammo_9x19_fmj", quantity: 60 },
+  { slotId: "stash_005", itemId: "armor_defender_iv", quantity: 1 },
+  { slotId: "stash_006", itemId: "helmet_fast_mt", quantity: 1 },
+  { slotId: "stash_007", itemId: "med_ifak", quantity: 2 },
+  { slotId: "stash_008", itemId: "med_bandage", quantity: 4 },
+  { slotId: "stash_009", itemId: "att_red_dot", quantity: 1 },
+  { slotId: "stash_010", itemId: "part_copper_wire", quantity: 5 },
+  { slotId: "stash_011", itemId: "part_circuit_board", quantity: 2 },
+  { slotId: "stash_012", itemId: "part_industrial_fan", quantity: 1 },
+  { slotId: "stash_013", itemId: "seed_greenhouse_a", quantity: 3 },
+  { slotId: "stash_014", itemId: "val_graphics_card", quantity: 1 },
+  { slotId: "stash_015", itemId: "key_warehouse_2a", quantity: 1 },
+  { slotId: "stash_016", itemId: "quest_radio_unit", quantity: 1 },
+];
