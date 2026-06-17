ea544ea975b103ec41064d867699f4ef66a0dbd1
Doeks050 <nicktalens@outlook.com>
2026-06-17 21:26:30 +0200

    Use central state on hideout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,19 @@ import { HideoutModuleGrid } from "../components/hideout/HideoutModuleGrid";
 import { LastRaidPanel } from "../components/hideout/LastRaidPanel";
 import { OperatorPanel } from "../components/hideout/OperatorPanel";
 import { SkillStrip } from "../components/hideout/SkillStrip";
-import { hideoutModules } from "../data/hideoutModules";
-import { operatorProfile } from "../data/operator";
+import { gameState } from "../data/gameState";
 
 export default function HomePage() {
   return (
     <GameShell title="Hideout" eyebrow="Home Base">
       <div className="grid h-full grid-rows-[auto_auto_1fr_auto] gap-2">
-        <OperatorPanel operator={operatorProfile} />
-        <SkillStrip skills={operatorProfile.skills} />
-        <HideoutModuleGrid modules={hideoutModules} />
+        <OperatorPanel operator={gameState.operator} />
+        <SkillStrip skills={gameState.operator.skills} />
+        <HideoutModuleGrid modules={gameState.hideoutModules} />
 
         <div className="grid grid-cols-2 gap-2">
-          <LastRaidPanel raid={operatorProfile.lastRaid} />
-          <ActiveTaskPanel task={operatorProfile.activeTask} />
+          <LastRaidPanel raid={gameState.operator.lastRaid} />
+          <ActiveTaskPanel task={gameState.operator.activeTask} />
         </div>
       </div>
     </GameShell>
