64df09a136bf5adb15332a09293b7d787e14d8f8
Doeks050 <nicktalens@outlook.com>
2026-06-18 15:58:22 +0200

    Use hideout page client state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
+import { HideoutPageClient } from "../components/hideout/HideoutPageClient";
 import { GameShell } from "../components/shell/GameShell";
-import { ActiveTaskPanel } from "../components/hideout/ActiveTaskPanel";
-import { HideoutModuleGrid } from "../components/hideout/HideoutModuleGrid";
-import { LastRaidPanel } from "../components/hideout/LastRaidPanel";
-import { OperatorPanel } from "../components/hideout/OperatorPanel";
-import { SkillStrip } from "../components/hideout/SkillStrip";
-import { gameState } from "../data/gameState";
 
 export default function HomePage() {
   return (
     <GameShell title="Hideout" eyebrow="Home Base">
-      <div className="grid h-full grid-rows-[auto_auto_1fr_auto] gap-2">
-        <OperatorPanel operator={gameState.operator} />
-        <SkillStrip skills={gameState.operator.skills} />
-        <HideoutModuleGrid modules={gameState.hideoutModules} />
-
-        <div className="grid grid-cols-2 gap-2">
-          <LastRaidPanel raid={gameState.operator.lastRaid} />
-          <ActiveTaskPanel task={gameState.operator.activeTask} />
-        </div>
-      </div>
+      <HideoutPageClient />
     </GameShell>
   );
 }
