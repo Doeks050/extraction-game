e409d41ee4a289f14145eef15211c547d7ffbad6
Doeks050 <nicktalens@outlook.com>
2026-06-18 15:58:27 +0200

    Use stash page client state

--- src/app/stash/page.tsx
+++ src/app/stash/page.tsx
@@ -1,32 +1,10 @@
 import { GameShell } from "../../components/shell/GameShell";
-import { StashClient } from "../../components/stash/StashClient";
-import { StashStatsPanel } from "../../components/stash/StashStatsPanel";
-import { gameState } from "../../data/gameState";
-import {
-  formatCredits,
-  formatWeight,
-  getInventoryValue,
-  getInventoryWeight,
-} from "../../lib/items";
+import { StashPageClient } from "../../components/stash/StashPageClient";
 
 export default function StashPage() {
-  const totalValue = getInventoryValue(gameState.stash);
-  const totalWeight = getInventoryWeight(gameState.stash);
-  const usedSlots = gameState.stash.length;
-  const maxSlots = 40;
-
   return (
     <GameShell title="Stash" eyebrow="Storage">
-      <div className="grid h-full grid-rows-[auto_1fr] gap-2">
-        <StashStatsPanel
-          usedSlots={usedSlots}
-          maxSlots={maxSlots}
-          totalValue={formatCredits(totalValue)}
-          totalWeight={formatWeight(totalWeight)}
-        />
-
-        <StashClient slots={gameState.stash} />
-      </div>
+      <StashPageClient />
     </GameShell>
   );
 }
