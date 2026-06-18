5310c095c76cbde86bfa5aa8c95255f236fadba0
Doeks050 <nicktalens@outlook.com>
2026-06-18 15:54:36 +0200

    Wrap app with game state provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GameStateProvider } from "../components/state/GameStateProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <GameStateProvider>{children}</GameStateProvider>
+      </body>
     </html>
   );
 }
