9690f1425a818351688d2a644b4886c8a09cbf2f
Doeks050 <nicktalens@outlook.com>
2026-06-18 17:43:43 +0200

    Remove home skill strip from hideout overview

--- src/components/hideout/HideoutPageClient.tsx
+++ src/components/hideout/HideoutPageClient.tsx
@@ -5,15 +5,13 @@ import { ActiveTaskPanel } from "./ActiveTaskPanel";
 import { HideoutModuleGrid } from "./HideoutModuleGrid";
 import { LastRaidPanel } from "./LastRaidPanel";
 import { OperatorPanel } from "./OperatorPanel";
-import { SkillStrip } from "./SkillStrip";
 
 export function HideoutPageClient() {
   const { state } = useGameState();
 
   return (
-    <div className="grid h-full grid-rows-[auto_auto_1fr_auto] gap-2">
+    <div className="grid h-full grid-rows-[auto_1fr_auto] gap-2">
       <OperatorPanel operator={state.operator} />
-      <SkillStrip skills={state.operator.skills} />
       <HideoutModuleGrid modules={state.hideoutModules} />
 
       <div className="grid grid-cols-2 gap-2">
