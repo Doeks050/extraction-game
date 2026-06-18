c23fda103eb0282b8402dae3fad02a35da543c7b
Doeks050 <nicktalens@outlook.com>
2026-06-18 15:59:07 +0200

    Use loadout page client state

--- src/app/loadout/page.tsx
+++ src/app/loadout/page.tsx
@@ -1,34 +1,10 @@
-import { AmmoReservePanel } from "../../components/loadout/AmmoReservePanel";
-import { DeployButton } from "../../components/loadout/DeployButton";
-import { LoadoutEquipmentPanel } from "../../components/loadout/LoadoutEquipmentPanel";
-import { LoadoutStatsPanel } from "../../components/loadout/LoadoutStatsPanel";
-import { QuickUsePanel } from "../../components/loadout/QuickUsePanel";
-import { RaidReadinessPanel } from "../../components/loadout/RaidReadinessPanel";
+import { LoadoutPageClient } from "../../components/loadout/LoadoutPageClient";
 import { GameShell } from "../../components/shell/GameShell";
-import { gameState } from "../../data/gameState";
-import { calculateLoadoutStats } from "../../lib/loadout";
 
 export default function LoadoutPage() {
-  const stats = calculateLoadoutStats(gameState.loadout);
-
   return (
     <GameShell title="Loadout" eyebrow="Deployment Prep">
-      <div className="grid h-full grid-rows-[1fr_auto] gap-2">
-        <div className="grid min-h-0 grid-cols-[1.25fr_0.95fr] gap-2">
-          <LoadoutEquipmentPanel slots={gameState.loadout.equipment} />
-
-          <div className="grid min-h-0 grid-rows-[auto_auto_1fr] gap-2">
-            <LoadoutStatsPanel stats={stats} />
-            <AmmoReservePanel ammoReserve={gameState.loadout.ammoReserve} />
-            <RaidReadinessPanel stats={stats} />
-          </div>
-        </div>
-
-        <div className="grid gap-2">
-          <QuickUsePanel quickSlots={gameState.loadout.quickSlots} />
-          <DeployButton />
-        </div>
-      </div>
+      <LoadoutPageClient />
     </GameShell>
   );
 }
