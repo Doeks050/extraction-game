6461845735f63fb5aa35e0b9e64401650fb7aa9b
Doeks050 <nicktalens@outlook.com>
2026-06-19 15:13:25 +0200

    Merge operator containers into equipment panel

--- src/components/loadout/LoadoutPageClient.tsx
+++ src/components/loadout/LoadoutPageClient.tsx
@@ -3,17 +3,16 @@
 import { useGameState } from "../state/GameStateProvider";
 import { DeployButton } from "./DeployButton";
 import { LoadoutEquipmentPanel } from "./LoadoutEquipmentPanel";
-import { OperatorContainersPanel } from "./OperatorContainersPanel";
 
 export function LoadoutPageClient() {
   const { state } = useGameState();
 
   return (
     <div className="grid h-full grid-rows-[1fr_auto] gap-2">
-      <div className="grid min-h-0 gap-2 overflow-y-auto">
-        <LoadoutEquipmentPanel slots={state.loadout.equipment} />
-        <OperatorContainersPanel containers={state.operator.containers} />
-      </div>
+      <LoadoutEquipmentPanel
+        slots={state.loadout.equipment}
+        containers={state.operator.containers}
+      />
 
       <DeployButton />
     </div>
