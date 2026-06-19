cbc0ff8306f4615c812931acea43fc76b6908064
Doeks050 <nicktalens@outlook.com>
2026-06-19 15:08:30 +0200

    Simplify loadout page layout

--- src/components/loadout/LoadoutPageClient.tsx
+++ src/components/loadout/LoadoutPageClient.tsx
@@ -1,34 +1,21 @@
 "use client";
 
-import { calculateLoadoutStats } from "../../lib/loadout";
 import { useGameState } from "../state/GameStateProvider";
-import { AmmoReservePanel } from "./AmmoReservePanel";
 import { DeployButton } from "./DeployButton";
 import { LoadoutEquipmentPanel } from "./LoadoutEquipmentPanel";
-import { LoadoutStatsPanel } from "./LoadoutStatsPanel";
-import { QuickUsePanel } from "./QuickUsePanel";
-import { RaidReadinessPanel } from "./RaidReadinessPanel";
+import { OperatorContainersPanel } from "./OperatorContainersPanel";
 
 export function LoadoutPageClient() {
   const { state } = useGameState();
-  const stats = calculateLoadoutStats(state.loadout);
 
   return (
     <div className="grid h-full grid-rows-[1fr_auto] gap-2">
-      <div className="grid min-h-0 grid-cols-[1.25fr_0.95fr] gap-2">
+      <div className="grid min-h-0 gap-2 overflow-y-auto">
         <LoadoutEquipmentPanel slots={state.loadout.equipment} />
-
-        <div className="grid min-h-0 grid-rows-[auto_auto_1fr] gap-2">
-          <LoadoutStatsPanel stats={stats} />
-          <AmmoReservePanel ammoReserve={state.loadout.ammoReserve} />
-          <RaidReadinessPanel stats={stats} />
-        </div>
+        <OperatorContainersPanel containers={state.operator.containers} />
       </div>
 
-      <div className="grid gap-2">
-        <QuickUsePanel quickSlots={state.loadout.quickSlots} />
-        <DeployButton />
-      </div>
+      <DeployButton />
     </div>
   );
 }
