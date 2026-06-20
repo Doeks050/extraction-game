54d93bc7fae06a6d6a16f10e2eb8870d1c53a89c
Doeks050 <nicktalens@outlook.com>
2026-06-20 10:54:50 +0200

    Add weapon class types

--- src/types/weapons.ts
+++ src/types/weapons.ts
@@ -1,3 +1,11 @@
+export type WeaponClassId =
+  | "assault_rifle"
+  | "battle_rifle"
+  | "smg"
+  | "pistol"
+  | "sniper"
+  | "machine_gun";
+
 export type WeaponAttachmentSlotId =
   | "optic"
   | "muzzle"
@@ -6,10 +14,21 @@ export type WeaponAttachmentSlotId =
   | "grip"
   | "stock"
   | "handguard"
-  | "light_laser";
+  | "light_laser"
+  | "bipod";
 
 export type WeaponAttachmentSlot = {
   id: WeaponAttachmentSlotId;
   label: string;
   equippedItemId?: string;
+};
+
+export type WeaponClassConfig = {
+  id: WeaponClassId;
+  label: string;
+  visualGridSize: {
+    width: number;
+    height: number;
+  };
+  attachmentSlotIds: WeaponAttachmentSlotId[];
 };
