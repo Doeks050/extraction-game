b1f644aa0f76d70d72d4e05cd7bb4f888791ed86
Doeks050 <nicktalens@outlook.com>
2026-06-21 21:07:23 +0200

    Register 7.62x51 DMR pack

--- src/data/items/weapons.ts
+++ src/data/items/weapons.ts
@@ -2,9 +2,15 @@ import { resolveWeaponAssetPath } from "../../lib/weaponAssetPath";
 import type { GameItem } from "../../types/items";
 import { assaultRifles } from "./weaponPacks/assaultRifles";
 import { assaultRifles762 } from "./weaponPacks/assaultRifles762";
+import { dmrs762 } from "./weaponPacks/dmrs762";
 import { pistols } from "./weaponPacks/pistols";
 
-const weaponPacks: GameItem[] = [...assaultRifles, ...assaultRifles762, ...pistols];
+const weaponPacks: GameItem[] = [
+  ...assaultRifles,
+  ...assaultRifles762,
+  ...dmrs762,
+  ...pistols,
+];
 
 export const weapons: GameItem[] = weaponPacks.map((weapon) => ({
   ...weapon,
