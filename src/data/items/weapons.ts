bf9e219027b1cb323cf2be2d661551dce8429e98
Doeks050 <nicktalens@outlook.com>
2026-06-21 20:54:18 +0200

    Register 7.62x51 assault rifle pack

--- src/data/items/weapons.ts
+++ src/data/items/weapons.ts
@@ -1,9 +1,10 @@
 import { resolveWeaponAssetPath } from "../../lib/weaponAssetPath";
 import type { GameItem } from "../../types/items";
 import { assaultRifles } from "./weaponPacks/assaultRifles";
+import { assaultRifles762 } from "./weaponPacks/assaultRifles762";
 import { pistols } from "./weaponPacks/pistols";
 
-const weaponPacks: GameItem[] = [...assaultRifles, ...pistols];
+const weaponPacks: GameItem[] = [...assaultRifles, ...assaultRifles762, ...pistols];
 
 export const weapons: GameItem[] = weaponPacks.map((weapon) => ({
   ...weapon,
