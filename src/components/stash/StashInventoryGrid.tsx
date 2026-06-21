26bca4b250dadb57af2ac18de862d57f8676ce94
Doeks050 <nicktalens@outlook.com>
2026-06-21 13:52:50 +0200

    Scale assault rifle images larger in stash

--- src/components/stash/StashInventoryGrid.tsx
+++ src/components/stash/StashInventoryGrid.tsx
@@ -38,7 +38,7 @@ function getWeaponImageClassName(slot: HydratedInventorySlot) {
   }
 
   if (weaponClass?.id === "assault_rifle") {
-    return "h-[135%] w-[135%] max-h-none max-w-none object-contain opacity-95";
+    return "h-[185%] w-[185%] max-h-none max-w-none object-contain opacity-95";
   }
 
   return "h-full w-full max-h-full max-w-full object-contain opacity-95";
