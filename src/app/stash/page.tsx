7041809436574d9f7c794e086e4b215d504ac9cb
Doeks050 <nicktalens@outlook.com>
2026-06-21 21:35:16 +0200

    Set stash title to orange

--- src/app/stash/page.tsx
+++ src/app/stash/page.tsx
@@ -3,7 +3,13 @@ import { StashPageClient } from "../../components/stash/StashPageClient";
 
 export default function StashPage() {
   return (
-    <GameShell title="Stash" eyebrow="" showSaveStatus={false} compactHeader>
+    <GameShell
+      title="Stash"
+      eyebrow=""
+      showSaveStatus={false}
+      compactHeader
+      titleClassName="text-orange-400"
+    >
       <StashPageClient />
     </GameShell>
   );
