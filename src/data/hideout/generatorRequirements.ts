e0e040d20927d1a7f79b0e9a11e8cccbaef9a34e
Doeks050 <nicktalens@outlook.com>
2026-06-23 13:37:02 +0200

    Rename generator mechanical requirement

--- src/data/hideout/generatorRequirements.ts
+++ src/data/hideout/generatorRequirements.ts
@@ -5,5 +5,5 @@ export const GENERATOR_LEVEL_ONE_DURATION_SECONDS = 30;
 export const generatorLevelOneRequirements: HideoutItemRequirement[] = [
   { itemId: "part_electric_motor", quantity: 1 },
   { itemId: "part_electrical_components", quantity: 2 },
-  { itemId: "part_mechanical_parts", quantity: 2 },
+  { itemId: "part_mechanical_components", quantity: 2 },
 ];
