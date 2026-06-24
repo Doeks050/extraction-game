de8a09d8ecf0314defe0ad8e7dc652cd509add3a
Doeks050 <nicktalens@outlook.com>
2026-06-24 15:21:22 +0200

    Add shared hideout crafting recipe type

---
+++ src/types/hideoutCrafting.ts
@@ -0,0 +1,16 @@
+export type HideoutCraftingInput = {
+  itemId: string;
+  quantity: number;
+};
+
+export type HideoutCraftingRecipe = {
+  id: string;
+  name: string;
+  requiredLevel: number;
+  durationSeconds: number;
+  inputs: HideoutCraftingInput[];
+  output: {
+    itemId: string;
+    quantity: number;
+  };
+};
