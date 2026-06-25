845cb3221b3c8771909b0f5187d3dcfb38963368
Doeks050 <nicktalens@outlook.com>
2026-06-25 09:23:18 +0200

    Show generator speed bonus on workbench

--- src/components/hideout/WorkbenchCraftingPanel.tsx
+++ src/components/hideout/WorkbenchCraftingPanel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { workbenchRecipes } from "../../data/hideout/workbenchRecipes";
+import { WORKBENCH_POWERED_DURATION_MULTIPLIER } from "../../lib/generatorStation";
 import type { HideoutModule } from "../../types/game";
 import type { InventorySlot } from "../../types/items";
 import { CraftingRecipesPanel } from "./CraftingRecipesPanel";
@@ -8,12 +9,14 @@ import { CraftingRecipesPanel } from "./CraftingRecipesPanel";
 type WorkbenchCraftingPanelProps = {
   module: HideoutModule;
   stash: InventorySlot[];
+  generatorPoweredOn: boolean;
   onCraft: (recipeId: string) => void;
 };
 
 export function WorkbenchCraftingPanel({
   module,
   stash,
+  generatorPoweredOn,
   onCraft,
 }: WorkbenchCraftingPanelProps) {
   return (
@@ -21,6 +24,10 @@ export function WorkbenchCraftingPanel({
       module={module}
       stash={stash}
       recipes={workbenchRecipes}
+      durationMultiplier={
+        generatorPoweredOn ? WORKBENCH_POWERED_DURATION_MULTIPLIER : 1
+      }
+      activeBonusLabel={generatorPoweredOn ? "25% faster" : undefined}
       onCraft={onCraft}
     />
   );
