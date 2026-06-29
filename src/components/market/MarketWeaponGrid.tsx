3322cd29438ce77112fb486a59ee0573ecf9a23c
Doeks050 <nicktalens@outlook.com>
2026-06-29 13:15:58 +0200

    Fine tune one-slot market icon scale

--- src/components/market/MarketWeaponGrid.tsx
+++ src/components/market/MarketWeaponGrid.tsx
@@ -87,7 +87,7 @@ function getItemImageClassName(item: GameItem, isOneSlotItem: boolean) {
   }
 
   if (isOneSlotItem) {
-    return "h-[115%] w-[115%] max-h-none max-w-none object-contain opacity-95";
+    return "h-[110%] w-[110%] max-h-none max-w-none object-contain opacity-95";
   }
 
   return "h-full w-full max-h-full max-w-full object-contain p-1 opacity-95";
